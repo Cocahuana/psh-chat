05fdb8620cf0e531ebce79f7691b9fbf945388aa
cocahuana <ezequieldominguez11@gmail.com>
2024-07-08 11:13:42 -0300

    Fix: colors and feat: add elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
 import "./App.css";
-
+import { Box, Flex } from "./components/elements";
+import styled from "styled-components";
+// const ChatsSection = styled.div``;
+// const ChattingSection = styled.div``;
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  @media (max-width: 480px) {
+    background-color: red;
+  }
+  @media (min-width: 481px) and (max-width: 768px) {
+    background-color: green;
+  }
+  @media (min-width: 769px) {
+    background-color: blue;
+  }
+`;
 function App() {
   return (
-    <>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div>Detail - Messages -</div>
-    </>
+    <Container>
+      <Flex>
+        <Box>
+          <nav>
+            <ul>
+              <li>
+                <a href={`/contacts/1`}>Your Name</a>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </Box>
+        <Box>Detail - Messages -</Box>
+      </Flex>
+    </Container>
   );
 }
 
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,19 +1,10 @@
-import React from "react";
 import App from "../App";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<App />} />
-      {/* 
-      <Route path="/" element={<Home />} />
-      <Route path="/categories/" element={<Categories />}>
-        <Route path="desktops" element={<Desktops />} />
-        <Route path="laptops" element={<Laptops />} />
-      </Route>
-      <Route path="/products" element={<Products />} /> 
-      */}
     </Routes>
   );
 }
